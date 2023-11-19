b93aa8ce22fa18a98bca13bbea9385ffb9269fec
niltatl <145554133+niltatl@users.noreply.github.com>
2023-11-19 17:41:33 +0300

    project1
    react ile yazıldı

---
+++ game/src/components/TotalScore.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const TotalScore = () => {
+  return (
+   <ScoreContainer>
+     <h1>0</h1>
+     <p>Total Score</p>
+     </ScoreContainer>
+  );
+};
+
+export default TotalScore;
+
+const ScoreContainer = styled.div`
+  max-width: 200px;
+  text-align: center;
+  h1 {
+    font-size: 100px;
+  }
+  p {
+    font-size: 24px;
+    font-weight: 500px;
+  }
+`;
